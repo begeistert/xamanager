5bc03dd9693ecd1bb3d74dd251b34273dc1df3a9
Iván Montiel Cardona <ivan_montiel2013@hotmail.com>
2024-01-28 00:10:00 -0600

    Initial Commit

---
+++ xamanager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  xamanager
+//
+//  Created by Ivan Montiel Cardona on 28/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ xamanager/xamanagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  xamanagerApp.swift
+//  xamanager
+//
+//  Created by Ivan Montiel Cardona on 28/01/24.
+//
+
+import SwiftUI
+
+@main
+struct xamanagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
